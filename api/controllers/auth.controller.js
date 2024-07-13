cc2a7b30688fcbacaaf2b56909edb47a82e71e3d
Danli Denny Deng <danlidennydeng@gmail.com>
2024-07-13 07:32:37 -0700

    added isAdmin field with default value to false in user.model.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -52,7 +52,10 @@ export const signin = async (req, res, next) => {
     // const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
     //   expiresIn: "1h"}
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
 
     // const { password: pass, ...rest } = validUser._doc;
 
