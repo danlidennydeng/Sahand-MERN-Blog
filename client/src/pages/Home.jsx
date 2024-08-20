6a908f222769d577f899ca956c45e5ddd62ed97a
Danli Denny Deng <danlidennydeng@gmail.com>
2024-08-19 20:53:52 -0700

    modified Home.jsx for render branch

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,9 +19,9 @@ export default function Home() {
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
         <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
         <p className="text-gray-500 text-xs sm:text-sm">
-          (This is for render branch) Here you'll find a variety of articles and
-          tutorials on topics such as web development, software engineering, and
-          programming languages.
+          <span className="text-red-700">(This is for render branch)</span> Here
+          you'll find a variety of articles and tutorials on topics such as web
+          development, software engineering, and programming languages.
         </p>
         <Link
           to="/search"
