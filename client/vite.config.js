8f202d777d8fb7cd0fdb7cbe1ab62bc5fdeadc3f
Danli Denny Deng <danlidennydeng@gmail.com>
2024-08-24 07:25:00 -0700

    external bcryptjs in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["bcryptjs"],
+    },
+  },
+
   server: {
     proxy: {
       "/api": {
