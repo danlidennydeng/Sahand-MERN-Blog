27299deaacd863fdac2dfc94a2824770687249fc
Danli Denny Deng <danlidennydeng@gmail.com>
2024-08-31 02:51:25 -0700

    Home.jsx minor fix again, pipeline to backend is working

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   useEffect(() => {
     const fetchPosts = async () => {
       // const res = await fetch(`${apiUrl}/api/post/getPosts`);
-      const res = await fetch("/api/post/getPosts");
+      const res = await fetch("/api/post/getposts");
       const data = await res.json();
       setPosts(data.posts);
     };
